a6acbf70b3131d694a82900775b25042684235b2
wishba <wisnubayu34@gmail.com>
2023-01-03 16:21:13 +0700

    click, hover, click outside the menu

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const menuOpen = () => document.getElementById('headerMenu').classList.add('header__menu--opened')
-const menuClose = () => document.getElementById('headerMenu').classList.remove('header__menu--opened')
+const menu = document.getElementById('headerMenu')
+const menuOpen = () => {
+  menu.classList.add('header__menu--opened')
+}
+const menuClose = () => {
+  menu.classList.remove('header__menu--opened')
+}
+
 
 const feature = document.getElementById('moreFeatures')
 const moreFeature = () => {
-  if (feature.className === 'header__more') {
-    feature.classList.add('header__more--open')
-  } else if (feature.className === 'header__more header__more--open') {
-    feature.classList.remove('header__more--open')
-  }
+  feature.classList.toggle('header__more--open')
 }
 const hoverInFeature = () => {
   feature.classList.add('header__more--open')
@@ -16,13 +18,10 @@ const hoverOutFeature = () => {
   feature.classList.remove('header__more--open')
 }
 
+
 const company = document.getElementById('moreCompany')
 const moreCompany = () => {
-  if (company.className === 'header__more') {
-    company.classList.add('header__more--open')
-  } else if (company.className === 'header__more header__more--open') {
-    company.classList.remove('header__more--open')
-  }
+  company.classList.toggle('header__more--open')
 }
 const hoverInCompany = () => {
   company.classList.add('header__more--open')
@@ -31,3 +30,9 @@ const hoverOutCompany = () => {
   company.classList.remove('header__more--open')
 }
 
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('header__menu') && event.target.classList.contains('header__menu--opened')) {
+    event.target.classList.remove('header__menu--opened')
+  }
+})
